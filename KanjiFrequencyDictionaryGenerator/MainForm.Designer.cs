3858de6453e0275141b01c058df208e7ca48840e
MaZaiPC <and-ivant@yandex.ru>
2017-02-28 12:36:54 +0300

    Added

---
+++ KanjiFrequencyDictionaryGenerator/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Moreniell.KanjiFrequencyDictionaryGenerator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxIn = new System.Windows.Forms.TextBox();
+			this.textBoxOut = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// textBoxIn
+			// 
+			this.textBoxIn.Location = new System.Drawing.Point(16, 40);
+			this.textBoxIn.Name = "textBoxIn";
+			this.textBoxIn.Size = new System.Drawing.Size(312, 20);
+			this.textBoxIn.TabIndex = 0;
+			// 
+			// textBoxOut
+			// 
+			this.textBoxOut.Location = new System.Drawing.Point(16, 104);
+			this.textBoxOut.Name = "textBoxOut";
+			this.textBoxOut.Size = new System.Drawing.Size(312, 20);
+			this.textBoxOut.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(16, 88);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Output:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 24);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(34, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Input:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(336, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Open";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.inputBtn_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(336, 104);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Choose";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.outputBtn_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(184, 176);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Convert!";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.convertButton_Click);
+			// 
+			// saveFileDialog
+			// 
+			this.saveFileDialog.Filter = "Файлы txt|*.txt";
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.Filter = "Файлы txt|*.txt";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(440, 217);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxOut);
+			this.Controls.Add(this.textBoxIn);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Преобразовать в частотный словарь кандзи";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxIn;
+		private System.Windows.Forms.TextBox textBoxOut;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
---
+++ KanjiFrequencyDictionaryGenerator/MainForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Moreniell.KanjiFrequencyDictionaryGenerator
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void inputBtn_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				textBoxIn.Text = openFileDialog.FileName;
+			}
+		}
+
+		private void outputBtn_Click(object sender, EventArgs e)
+		{
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				textBoxOut.Text = saveFileDialog.FileName;
+			}
+		}
+
+		private void convertButton_Click(object sender, EventArgs e)
+		{
+			if (File.Exists(textBoxIn.Text))
+			{
+				string content = File.ReadAllText(textBoxIn.Text);
+				var frequencyDic = new Dictionary<char, int>();
+
+				foreach (char c in content)
+				{
+					if (!char.IsDigit(c)
+						/* Latina   */ && !"abcdefghijklmnopqrstuvwxyz".Contains(char.ToLower(c))
+						/* Romanji  */ && !"ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ".Contains(char.ToUpper(c))
+						/* Hiragana */ && !"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをん".Contains(c)
+						/* Katakana */ && !"アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲン".Contains(c)
+						/* Hiragana 半濁点 */ && !"がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょっ".Contains(c)
+						/* Katakana 半濁点 */ && !"ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポャュョィッァェォ".Contains(c)
+						/* Kanji Numbers */ && !"〇零一二三四五六七八九十百万億挑".Contains(c)
+						/* Symbols       */ && !" */!.,()[]?\"\n\r'{}:%$#@~`-+=＝_×・&、。°→！■？|>／【】「」〜～◇◆▲▼－〈〉（）『』“　”％…♪★©ー々".Contains(c))
+					{
+						if (!frequencyDic.Keys.Contains(c)) frequencyDic[c] = 0;
+						frequencyDic[c]++;
+					}
+				}
+				if (!string.IsNullOrWhiteSpace(textBoxOut.Text))
+				{
+					bool allowMerging = false;
+					
+					if (File.Exists(textBoxOut.Text))
+					{
+						if (File.ReadAllLines(textBoxOut.Text)[0].Contains("[allow-merge]"))
+						{
+							allowMerging = true;
+							frequencyDic = MergeWithFile(frequencyDic, textBoxOut.Text);
+						}
+						File.Delete(textBoxOut.Text);
+					}
+
+					// Save dic in file
+					using (var sw = new StreamWriter(File.OpenWrite(textBoxOut.Text), Encoding.Unicode))
+					{
+						if (allowMerging) sw.WriteLine("[allow-merge]");
+						foreach (var kv in frequencyDic.OrderByDescending(x => x.Value))
+						{
+							sw.WriteLine(kv.Value + ": " + kv.Key);
+						}
+					}
+
+					MessageBox.Show(allowMerging ? "Done! (merged)" : "Done!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else
+				{
+					MessageBox.Show("Error, output path is empty!");
+				}
+			}
+			else
+			{
+				MessageBox.Show("Error, input file is not found!");
+			}
+		}
+
+		private Dictionary<char, int> MergeWithFile(Dictionary<char, int> dic, string fileName)
+		{
+			string[] lines = File.ReadAllLines(fileName);
+
+			foreach (string line in lines)
+			{
+				int index = line.IndexOf(':');
+				if (index == -1) continue;
+
+				int value = int.Parse(line.Substring(0, index));
+				//MessageBox.Show($"До: {line}\nПосле: {value}");
+				char key = line.Substring(index + 2)[0];
+
+				if (!dic.Keys.Contains(key)) dic[key] = 0;
+				dic[key] += value;
+			}
+			return dic;
+		}
+	}
+}
